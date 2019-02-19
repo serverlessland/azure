8ed53830f85270855408f51784229e2f2d88ffee
Mikhail Shilkov <mikhail.shilkov@prodrive-technologies.com>
2019-02-19 13:36:44 +0100

    Map tiles with greyscale and saving to storage

--- http/v2/jsmaptiles/http/index.js
+++ http/v2/jsmaptiles/http/index.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const Stream = require('stream').Transform;
-
+const Jimp = require('jimp');
+const util = require('util');
 const envInstance = process.env["WEBSITE_INSTANCE_ID"];
 const instance = envInstance ? `AZ:${envInstance}` : "LOCAL:LOCAL";
 
@@ -24,9 +25,21 @@ async function downloadImage(url) {
 
 
 module.exports = async function(context) {
+  const isCold = count == 0;
   count += 1;
 
-  const image = await downloadImage('https://b.tile.openstreetmap.org/' + context.bindingData.path);
+  const url = 'https://b.tile.openstreetmap.org/' + context.bindingData.path;
+  let image = context.bindings.inputBlob;
+  if (!image) {
+    image = await downloadImage(url);
+    context.bindings.outputBlob = image;
+  }
+  if (isCold) {
+    const original = await Jimp.read(image);
+    const grey = await original.greyscale();
+    const getBufferAsync = util.promisify(grey.getBuffer.bind(grey));
+    image = await getBufferAsync(Jimp.MIME_PNG);
+  }
 
   context.res = { 
     status: 200, 
@@ -34,7 +47,7 @@ module.exports = async function(context) {
     isRaw: true,
     headers: {
         "Content-Type": "image/png",
-        "X-CB-Name": "Azure_JSNoop",
+        "X-CB-Name": "Azure_JSMapTile",
         "X-CB-Count": count,
         "X-CB-Instance": instance
     } 
